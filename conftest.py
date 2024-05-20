df07a7b83f9e81eca376c8b9410ca64b78c3eb33
oleuva <oleg.uvarov@sitronics-kt.ru>
2024-05-20 13:00:01 +0300

    diplom_2_2

--- conftest.py
+++ conftest.py
@@ -3,12 +3,12 @@ import requests
 
 from data.handlers import Handlers
 from data.urls import Urls
-from data.user_data import User
+from helpers import Helper
 
 
 @pytest.fixture(scope="function")
 def create_user():
-    payload = User.create_data_user()
+    payload = Helper.create_data_user()
     login_data = payload.copy()
     del login_data["name"]
     response = requests.post(f"{Urls.MAIN_URL}{Handlers.CREATE_USER}", data=payload)
